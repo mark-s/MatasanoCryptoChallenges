19c17ee9d952547e17ce24cd7dec7951aafc911f
Mark Staff <mark.a.staff@gmail.com>
2014-08-29 16:25:10 +0900

    Started on set 2 Challenge 1 Done

---
+++ SetTwo.Tests/One/PKCS7PaddingTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SetTwo.One;
+
+namespace SetTwo.Tests.One
+{
+    [TestFixture]
+    public class PKCS7PaddingTests
+    {
+        private PKCS7Padding _padder;
+
+        [SetUp]
+        public void Setup()
+        {
+            _padder = new PKCS7Padding();
+        }
+
+        [Test]
+        public void GetPaddedText_GivenText_PadsCorrectly()
+        {
+
+            // Arrange
+            const string TEST_STRING = "YELLOW SUBMARINE";
+            const string EXPECTED_STRING = @"YELLOW SUBMARINE\x04\x04\x04\x04";
+
+            // Act
+            string result = _padder.GetPaddedText(TEST_STRING, 20);
+
+
+            // Assert
+            result.Should().Be(EXPECTED_STRING);
+
+        }
+
+
+        [Test]
+        public void GetByteCount_GivernYELLOWSUBMARINE_Return16()
+        {
+
+            // From one of the previous challenges: "YELLOW SUBMARINE" [is] exactly 16 bytes long
+            
+            // Arrange
+            const string TEST_STRING = "YELLOW SUBMARINE";
+
+            // Act
+            var result = _padder.GetByteCount(TEST_STRING);
+
+            // Assert
+            result.Should().Be(16);
+
+        }
+
+
+}
+}
---
+++ SetTwo/One/PKCS7Padding.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace SetTwo.One
+{
+    public class PKCS7Padding
+    {
+
+        // See: http://www.wikiwand.com/en/Padding_(cryptography)
+        // and : http://tools.ietf.org/html/rfc2315
+        // For such algorithms, the method shall be to pad the input at the trailing end with:
+        //  k - (l mod k) octets all having value k - (l mod k), where l is the length of the input.
+
+
+        public string GetPaddedText(string textToPad, int blockLength)
+        {
+            var inputTextLengthInBytes = GetByteCount(textToPad);
+
+            if (inputTextLengthInBytes > 256 || blockLength > 256)
+                throw new ArgumentOutOfRangeException("blockLength", blockLength, "This padding method is well-defined if and only if k < 256");
+
+            var paddingBytesNeeded = (blockLength % inputTextLengthInBytes);
+
+            var paddingCountAsHexString = String.Format("\\x{0:x2}", paddingBytesNeeded);
+
+            var toAppend = String.Concat(Enumerable.Repeat(paddingCountAsHexString, paddingBytesNeeded));
+
+            return textToPad + toAppend;
+        }
+
+
+        public int GetByteCount(string stringTocount)
+        {
+            return Encoding.UTF8.GetByteCount(stringTocount);
+        }
+
+
+    }
+}
